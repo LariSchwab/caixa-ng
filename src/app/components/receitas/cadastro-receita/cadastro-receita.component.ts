0bc1679ee9508439c3c37e67bc1752d741be8135
LarissaSchwab <larissa.schwab@yahoo.com.br>
2025-04-25 21:55:05 -0300

    permitir cadastrar receita

---
+++ src/app/components/receitas/cadastro-receita/cadastro-receita.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro-receita',
+  imports: [FormsModule],
+  templateUrl: './cadastro-receita.component.html',
+  styleUrl: './cadastro-receita.component.css'
+})
+export class CadastroReceitaComponent {
+ 
+  nome: string= "";
+  valor: number = 0;
+  receitas: Array<string> = [];
+
+  salvarReceita(){
+    debugger;
+    this.receitas.push(this.nome);
+    alert(this.nome);
+  }
+}
