8ba32b9e99b5d120dbd2ae1e25abdfb6cb0a79a8
LarissaSchwab <larissa.schwab@yahoo.com.br>
2025-04-28 21:19:25 -0300

    adicionando lista de receitas, permitir apagar e editar

--- src/app/components/receitas/cadastro-receita/cadastro-receita.component.ts
+++ src/app/components/receitas/cadastro-receita/cadastro-receita.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Receita } from '../../../models/receita';
 
 @Component({
   selector: 'app-cadastro-receita',
@@ -8,14 +9,79 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './cadastro-receita.component.css'
 })
 export class CadastroReceitaComponent {
- 
+  proximoId: number = 0;
+
+  // idParaEditar é uma variável do tipo number que é nullable, ou seja, ela pode ou nao ter valor
+  idParaEditar?: number;
+
   nome: string= "";
   valor: number = 0;
-  receitas: Array<string> = [];
+  receitas: Array<Receita> = [];
 
   salvarReceita(){
-    debugger;
-    this.receitas.push(this.nome);
-    alert(this.nome);
+    if (this.nome.length < 3) {
+      alert("Nome deve conter no mínimo 3 caracteres")
+      return;
+    }
+    if (this.nome.length > 30) {
+      alert("Nome deve conter no máximo 30 caracteres")
+      return;
+    }
+
+    // "5490,29" => "5490.29", por isso é necessário fazer o replace
+    let valor = parseFloat(this.valor.toString().replace(",", "."));
+    // NaN é not a number, é recebido NaN quando o valor que é tentado converter para float 
+    // não é um float válido
+    if (Number.isNaN(valor)) {
+      alert("Valor deve ser um número real");
+      return;
+    }
+    
+    if (valor <= 0) {
+      alert("Valor deve ser maior que R$ 0,00");
+      return;
+    }
+
+    if (this.idParaEditar == undefined){
+      this.cadastrarReceita();
+    }else{
+      this.editarReceita();
+    }
+
+    //alert(this.nome);
+    this.nome = "";
+    this.valor = 0;
+  }
+
+  editarReceita(){
+    let indiceReceita = this.receitas.findIndex(x => x.id == this.idParaEditar);
+    this.receitas[indiceReceita].nome = this.nome;
+    this.receitas[indiceReceita].valor = this.valor;
+
+    this.idParaEditar = undefined;
+  }
+
+  cadastrarReceita(){
+    this.proximoId++;
+    let receita = new Receita(this.proximoId, this.nome, this.valor);
+    //debugger;
+    this.receitas.push(receita);
+  }
+
+  apagar(receita: Receita){
+    let confirmacao = confirm(`Deseja realmente apagar a receita '${receita.nome}'?`);
+    if (confirmacao == false)
+      return
+
+    // Buscando o indice da receita e filtrando por id da receita que foi selecionada
+    let indiceReceita = this.receitas.findIndex(x => x.id == receita.id);
+    // removendo a receita da lista receitas utilizando o indice, removendo 1 elemento da lista
+    this.receitas.splice(indiceReceita, 1);
+  }
+
+  editar(receita: Receita){
+    this.nome = receita.nome;
+    this.valor = receita.valor;
+    this.idParaEditar = receita.id;
   }
 }
