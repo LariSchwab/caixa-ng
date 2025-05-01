111b9dd435ac3f324e567a06793923d6ccc3b103
LarissaSchwab <larissa.schwab@yahoo.com.br>
2025-05-01 16:09:06 -0300

    adicionado campo categoria e quantidade aos produtos

--- src/app/components/produtos/cadastro-produto/cadastro-produto.component.ts
+++ src/app/components/produtos/cadastro-produto/cadastro-produto.component.ts
@@ -14,6 +14,8 @@ export class CadastroProdutoComponent {
   produtos: Array<Produto> = [];
   proximoId: number = 0;
   idParaEditar?: number;
+  categoria: string = "";
+  quantidade: number = 0;
 
   salvarProduto() {
     if (this.nome.length < 3) {
@@ -25,37 +27,67 @@ export class CadastroProdutoComponent {
       return;
     }
 
-    if (this.idParaEditar == undefined) {
-      this.cadastrarProduto();
-    } else {
-      this.editarProduto();
+    if (this.categoria.trim() === "") {
+      alert("Selecione uma categoria");
+      return;
     }
+
+    if (this.quantidade <= 0 || isNaN(this.quantidade)) {
+      alert("Selecione uma quantidade válida");
+      return;
+    }
+  // || é condição OU mas que é usada assim no TS e no JS
+  // isNaN verifica se variável não é um número válido
+
+  if (this.idParaEditar == undefined) {
+    this.cadastrarProduto();
+  } else {
+    this.editarProduto();
+  }
   }
 
   editarProduto(){
     let indiceProduto = this.produtos.findIndex(x => x.id == this.idParaEditar);
     this.produtos[indiceProduto].nome = this.nome;
+    this.produtos[indiceProduto].categoria = this.categoria;
+    this.produtos[indiceProduto].quantidade = this.quantidade;
 
     this.idParaEditar = undefined;
   }
 
   cadastrarProduto() {
     this.proximoId++;
-    let produto = new Produto(this.proximoId, this.nome);
+    let produto = new Produto(
+        this.proximoId, 
+        this.nome, 
+        this.categoria, 
+        this.quantidade);
+  
     this.produtos.push(produto);
+    
   }
 
   editar(produto: Produto) {
     this.nome = produto.nome;
+    this.categoria = produto.categoria;
     this.idParaEditar = produto.id;
+    this.quantidade = produto.quantidade;
   }
 
   apagar(produto: Produto) {
     let confirmacao = confirm(`Deseja realmente apagar o produto '${produto.nome}'?`);
-  if(confirmacao == false)
-    return
-  let indiceProduto = this.produtos.findIndex(x => x.id == produto.id);
-  this.produtos.splice(indiceProduto, 1);
+      if(confirmacao == false)
+        return
+    let indiceProduto = this.produtos.findIndex(x => x.id == produto.id);
+    this.produtos.splice(indiceProduto, 1);
+
+    let confirmacaoCategoria = confirm(`Deseja realmente apagar o produto '${produto.categoria}'?`);
+      if(confirmacaoCategoria == false)
+        return
+    
+    let confirmacaoQtidade = confirm(`Deseja realmente apagar o produto '${produto.quantidade}'?`);
+    if(confirmacaoQtidade == false)
+      return
 
   }
 }
--- src/app/models/produtos.ts
+++ src/app/models/produtos.ts
@@ -1,9 +1,13 @@
 export class Produto{
     id: number;
     nome: string;
+    categoria: string;
+    quantidade: number;
 
-    constructor(id:number, nome: string){
+    constructor(id:number, nome: string, categoria: string, quantidade:number){
         this.id = id;
         this.nome = nome;
+        this.categoria = categoria;
+        this.quantidade = quantidade;
     }
 }
